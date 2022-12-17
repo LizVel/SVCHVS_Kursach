2552ba9079e2506d008339de94852eca71f1bbc3
LizVel <Elizaveligurova@gmail.com>
2022-12-18 00:56:58 +0300

    init: create empty frontend project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            Empty project
+        </div>
+    );
+}
+
+export default App;
