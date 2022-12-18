44e222cba1aaaf6a9ecae53c88161796298728a9
LizVel <Elizaveligurova@gmail.com>
2022-12-18 20:27:42 +0300

    feat: add start page

--- frontend/src/pages/StartPage/StartPage.tsx
+++ frontend/src/pages/StartPage/StartPage.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./StartPage.scss";
+import {Link} from "react-router-dom";
 
 export function StartPage() {
   return (
       <div className="wrapper">
-          <main className={"start-page"}></main>
+          <main className={"start-page"}>
+              <div className={"start-page__container"}>
+                  <div className={"start-page__title"}>view</div>
+                  <div className={"start-page__description"}>Журнал о путешествиях и стиле</div>
+                  <Link to={`login`} className={"start-page__button"}>
+                      <span>read more</span>
+                  </Link>
+              </div>
+
+          </main>
       </div>
   );
 }
